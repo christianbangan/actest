7825c9818be12fb5d8365ef360856d7e80ae3186
Albert Christian Bangan <albert.bangan@ecpay.com.ph>
2024-06-16 11:56:10 +0800

    Enabled CORS

--- GeneratorAPI/Program.cs
+++ GeneratorAPI/Program.cs
@@ -5,6 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +23,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors("AllowAll");
 app.UseSwagger();
 app.UseSwaggerUI();
 
