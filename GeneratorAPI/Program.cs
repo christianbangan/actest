fd115df44229f446c22a504aca889feee94f77b9
christianbangan <albertchristianbangan@gmail.com>
2024-12-05 10:26:20 +0800

    Added application insights

--- GeneratorAPI/Program.cs
+++ GeneratorAPI/Program.cs
@@ -1,4 +1,5 @@
 using GeneratorAPI.ExtensionMethods;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
 builder.Inject();
+builder.Services.Configure<Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration>(config =>
+{
+config.SetAzureTokenCredential(new DefaultAzureCredential());
+});
+
+builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
+{
+    ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+});
 
 var app = builder.Build();
 
